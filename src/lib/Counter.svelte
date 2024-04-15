27547e83153d979b88f0af1430345757e2e27854
AngieCana <grilozangie@gmail.com>
2024-04-15 11:14:16 -0400

    first commit

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let initialCount = 0;
+  export let maxCount = undefined;
+
+  console.log($$props, $$restProps);
+
+  let count = initialCount;
+
+  function increment (){
+   if (count == maxCount) return;
+   count += 1;
+    
+  }
+
+</script>
+
+<button on:click={increment}>Clicks {count}</button>
+
+
+<style>
+  button{
+    padding: 30px 40px;
+  }
+</style>
